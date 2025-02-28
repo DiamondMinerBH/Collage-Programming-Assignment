aa4b0a574da8cd7f8b278589770d262faa4cf604
Diamond <b.hatch2008@outlook.com>
2025-02-28 13:50:50 +0000

    Worked on the individual teams registering and make the if statements better by adding loops

--- Main.java
+++ Main.java
@@ -1,36 +1,110 @@
 import java.util.*;
 public class Main {
-    public static void main (String[] args) {
-        registerTeams();
+    public static void main(String[] args) {
+        registerIndividualTeams();
     }
-    public static void registerTeams(){
-        HashMap<String, ArrayList<String>> teams = new HashMap<>();
-        for (int i = 0; i < 4; i++){
+
+    public static void registerTeams() {
+        HashMap<String, ArrayList<String>> GroupTeams = new HashMap<>();
+        for (int i = 0; i < 4; i++) {
             Scanner scanner = new Scanner(System.in);
-            System.out.println("Enter Team Name");
+            System.out.println("Enter Team Name: ");
             String teamName = scanner.nextLine();
 
-            teams.put(teamName, new ArrayList<>());
-            for (int j = 0; j < 5; j++){
+            GroupTeams.put(teamName, new ArrayList<>());
+            for (int j = 0; j < 5; j++) {
                 Scanner scanner1 = new Scanner(System.in);
-                System.out.println("Enter name ");
+                System.out.println("Enter name: ");
                 String memberName = scanner.nextLine();
-                teams.get(teamName).add(memberName);
+                GroupTeams.get(teamName).add(memberName);
 
             }
 
+            boolean check = true;
+            while (check) {
+                System.out.println("Do you want to play 1 game or all games: ");
+                String gameChoice = scanner.nextLine();
+                if (gameChoice.contains("1") || gameChoice.toLowerCase().contains("one")) {
+                    GroupTeams.get(teamName).add("1");
+                    check = false;
+                } else if (gameChoice.toLowerCase().contains("all")) {
+                    GroupTeams.get(teamName).add("0");
+                    check = false;
+                } else {
+                    System.out.println("Error Please Try Again");
+                }
+                System.out.println(GroupTeams);
 
-
-            System.out.println("Do you want to play 1 game or all games");
-            String gameChoice = scanner.nextLine();
-            if (gameChoice.contains("1") || gameChoice.toLowerCase().contains("one")){
-                teams.get(teamName).add("1");
-            } else {
-                teams.get(teamName).add("0");
             }
-            System.out.println(teams);
+
         }
 
+
+            }
+    public static void registerIndividualTeams(){
+        HashMap<String, ArrayList<String>> IndividualTeams = new HashMap<>();
+        for (int i = 0; i < 20; i++){
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter Your Name: ");
+            String individualName = scanner.nextLine();
+            IndividualTeams.put(individualName, new ArrayList<>());
+            IndividualTeams.get(individualName);
+
+            System.out.println("Do you want to play 1 game or all games: ");
+            String gameChoice = scanner.nextLine();
+            boolean check = true;
+            while (check){
+                if (gameChoice.contains("1") || gameChoice.toLowerCase().contains("one")) {
+                    IndividualTeams.get(individualName).add("One Game");
+
+                    boolean check1 = true;
+                    while (check1){
+                        System.out.println("""
+                        
+                        
+                        We have 5 different events please select one out of the 5 options by selecting the corresponding number
+                        1: Javelin
+                        2: Maths quiz
+                        3: Drawing Blindfolded
+                        4: Fitness Pacer Test
+                        5: Faster to build a house of cards
+                        """);
+                        String gameChoice1 = scanner.nextLine();
+                        if (gameChoice1.contains("1") || gameChoice.toLowerCase().contains("one")){
+                            IndividualTeams.get(individualName).add("javelin");
+                            check1 = false;
+
+                        } else if (gameChoice1.contains("2") || gameChoice1.toLowerCase().contains("two")) {
+                            IndividualTeams.get(individualName).add("maths quiz");
+                            check1 = false;
+
+                        } else if (gameChoice1.contains("3") || gameChoice1.toLowerCase().contains("three")) {
+                            IndividualTeams.get(individualName).add("drawing blindfolded");
+                            check1 = false;
+
+                        } else if (gameChoice1.contains("4") || gameChoice1.toLowerCase().contains("four")) {
+                            IndividualTeams.get(individualName).add("fitness pacer test");
+                            check1 = false;
+                        }else if (gameChoice1.contains("5") || gameChoice1.toLowerCase().contains("five")) {
+                            IndividualTeams.get(individualName).add("Fastest to build a house of cards");
+                            check1 = false;
+
+                        }else {
+                            System.out.println("Error please try again");
+
+                        }
+                    }
+                    check = false;
+                } else if (gameChoice.toLowerCase().contains("all")) {
+                    IndividualTeams.get(individualName).add("0");
+                    check = false;
+                } else {
+                    System.out.println("Error Please Try Again");
+                }
+        }
+
+            System.out.println(IndividualTeams);
+        }
     }
     }
 
